91a30f02c75b6283d6dfbab070ef3555bc81c8f9
--help <domsbuhendwa2@gmail.com>
2020-05-16 19:42:20 +0200

    fix bug, handle client object

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,8 +14,7 @@ function checkError(req) {
 module.exports = {
   adduser: async function (req, res) {
     try {
-      const { username, passwords, levels } = req.body;
-      console.log(req);
+      const { username, passwords, levels } = req.body.user;
       const item = new models.users({
         id: shortid.generate(),
         username: username,
